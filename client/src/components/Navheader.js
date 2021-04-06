0e4e67dd7be89e38e158a56439de63ad98d5d9eb
Paul Yi <paulyi@pauls-mbp.home>
2021-04-05 20:12:21 -0700

    aligned right on navbar links

--- client/src/components/Navheader.js
+++ client/src/components/Navheader.js
@@ -8,14 +8,14 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-boots
 
 const Navheader = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="justify-content-between">
     <Navbar.Brand href="#home">We Sell Things</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+      <Nav className="ml-auto justify-content-between mr-5">
+        <Nav.Link className="pr-5" href="#home">Home</Nav.Link>
+        <Nav.Link className="pr-5" href="#link">Link</Nav.Link>
+        <NavDropdown className="pr-5" title="Cart?" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -23,10 +23,10 @@ const Navheader = () => {
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown>
       </Nav>
-      <Form inline>
+      {/* <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="outline-success">Search</Button>
-      </Form>
+      </Form> */}
     </Navbar.Collapse>
   </Navbar>
 
